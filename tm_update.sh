7990d2351514fe982bab1e145906cc8f15d53696
a.chava <alexchav@gmail.com>
2025-12-23 14:58:51 +0300

    Python38

--- tm_update.sh
+++ tm_update.sh
@@ -10,6 +10,14 @@ git checkout master
 git fetch origin
 git reset --hard origin/master
 
+# Делаем все файлы в папке ./test исполняемыми
+echo "Предоставление прав на выполнение для всех файлов в папке ./test..."
+find ./test -type f -exec chmod +x {} \;
+
+# Делаем все .sh файлы в папке исполняемыми
+echo "Предоставление прав на выполнение для всех .sh файлов..."
+find /home/tm_rp -name "*.sh" -type f -exec chmod +x {} \;
+
 # Восстановить свои версии файлов
 cp config.json.backup config.json
 cp database.db.backup database.db
